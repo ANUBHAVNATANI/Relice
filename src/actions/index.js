b6c8a68f94aa7a32976f39e1dd7afbb500bb04f5
ANUBHAVNATANI <anubhavnatani@gmail.com>
2019-10-19 16:21:02 +0530

    LogoAdd

--- src/actions/index.js
+++
@@ -1,4 +0,0 @@
-export const selectImage = (image) => (dispatch) => {
-	dispatch({ type: 'SELECT_IMAGE', payload: image });
-};
-
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import selectImageReducer from './selectImageReducer';
-
-export default combineReducers({
-	selectedImage: selectImageReducer
-});
